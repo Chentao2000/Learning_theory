b5d4106b88b7a4826d0edb77b2289c77556151ae
Zhuomin Lin <98099819+mminminn@users.noreply.github.com>
2022-06-17 21:26:05 +0800

    Update 依次提x.c

--- M_数据结构/ch1/计算多项式/依次提x.c
+++ M_数据结构/ch1/计算多项式/依次提x.c
@@ -4,7 +4,7 @@ double f(int n, int a[], double x)
     int i;
     double p = a[n]; 
     for (i=n; i>0; i--)
-        p = a[i-1] + x*p; //从最里层开始算，每次更新p的值
+        p = a[i-1] + x*p; //p从an开始，每次更新p的值
     printf("%lf\n", p); 
 }
 int main()
