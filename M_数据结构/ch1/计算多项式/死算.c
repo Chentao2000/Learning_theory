b737c837b78e7fdba3c4b73dd03e76008bbca74c
Zhuomin Lin <98099819+mminminn@users.noreply.github.com>
2022-06-17 21:03:06 +0800

    Update 死算.c

--- M_数据结构/ch1/计算多项式/死算.c
+++ M_数据结构/ch1/计算多项式/死算.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-#include <math.h>   //调用了pow
+#include <math.h> //调用了pow
 double f(int n, int a[], double x)
 {
-    int i;
+    int i = 0;
     double p = a[0];
-    for(i=1; i<=n; i++)
+    for(i=1;i<=n;i++)
     {
-        p+=(a[i] * pow(x, i));  //pow(x, i)也就是x的i次方
+        p += a[i] * pow(x, i); //pow(x, i)也就是x的i次方
     }
-    printf("%lf\n", p);
+    printf("%lf\n", p); //打印小数用%lf
 }
 int main()
 {
-    int arr[] = {1, 2, 3, 4, 5};
-    f(10, arr, 2.0);
+    int arr[] = {1, 2, 3, 4, 5}; 
+    f(5, arr, 2);
     return 0;
 }
