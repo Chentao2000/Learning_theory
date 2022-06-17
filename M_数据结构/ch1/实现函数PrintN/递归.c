5ae4f8cdcc0d99472cbcb347ee6a67e45b91abea
Zhuomin Lin <98099819+mminminn@users.noreply.github.com>
2022-06-17 20:21:07 +0800

    Update 递归.c

--- M_数据结构/ch1/实现函数PrintN/递归.c
+++ M_数据结构/ch1/实现函数PrintN/递归.c
@@ -3,8 +3,8 @@ void PrintN(int N)
 {
     if(N) //如果N不为0
     {
-        PrintN(N-1);
-        printf("%d\n", N);
+        PrintN(N-1);  //100-1, 99-1, 98-1 ...
+        printf("%d\n", N); //100, 99, 98 ...
     }
 }
 int main()
