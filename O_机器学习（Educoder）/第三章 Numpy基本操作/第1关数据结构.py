c7b9c253cb21081d5c4d028dcd085d3b03d7722f
陈涛 <68007558+Chentao2000@users.noreply.github.com>
2022-08-17 23:56:59 +0800

    Create 第1关数据结构.py

---
+++ O_机器学习（Educoder）/第三章 Numpy基本操作/第1关数据结构.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+data = {
+       'a':{'A':90,'B':89,'C':78.0,'D':78.0},
+       'b':{'A':89,'B':89,'C':78.0,'D':78.0},
+       'c':{'A':78,'B':92}
+       }
+df1 = pd.DataFrame(data)
+print(df1)
+
+
+########## End ##########
+
